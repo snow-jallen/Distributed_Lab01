609dde6618d21401a94ef34afd3a4931de6a2baf
Jonathan Allen <jallen@snow.edu>
2019-03-27 16:05:49 -0600

    The basics work.

---
+++ Worker/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Distributed_Lab01
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var serviceCollection = new ServiceCollection();
+            Configure(serviceCollection);
+            var services = serviceCollection.BuildServiceProvider();
+            var httpClientFactory = services.GetRequiredService<IHttpClientFactory>();
+            var client = httpClientFactory.CreateClient();
+
+            Console.WriteLine("Sleeping...");
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+
+            var response = await client.GetStringAsync("http://server/api/values");
+            Console.WriteLine(response);
+        }
+
+        public static void Configure(IServiceCollection services)
+        {
+            services.AddHttpClient();
+        }
+    }
+}

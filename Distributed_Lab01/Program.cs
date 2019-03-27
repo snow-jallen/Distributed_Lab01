666d7fafe3e22e965ca01397bae641eac85ebcfb
Jonathan Allen <jallen@snow.edu>
2019-03-27 15:15:19 -0600

    Add project files.

---
+++ Distributed_Lab01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Distributed_Lab01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
